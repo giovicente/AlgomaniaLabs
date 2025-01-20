a53b303a54db0832c9d65062d2f7965a6e48a37d
Giovanni <gvn.gentile@gmail.com>
2025-01-20 00:55:22 -0300

    doc: Adding Javadoc to justify the choice of LinkedList

--- src/main/java/com/giovicente/alphabet/array/AlphabetArray.java
+++ src/main/java/com/giovicente/alphabet/array/AlphabetArray.java
@@ -11,6 +11,26 @@ public class AlphabetArray {
         System.out.println(returnAlphabetLetters((26)));
     }
 
+    /**
+     * We are using {@code LinkedList} because it is more efficient for insertions and deletions
+     * than for element lookups.
+     *
+     * <p>In this case, the goal is to perform insertions and then print the elements sequentially,
+     * making {@code LinkedList} more suitable than {@code ArrayList}. The {@code ArrayList} relies
+     * on an underlying array, which requires resizing and copying the array whenever an insertion
+     * or deletion occurs.
+     *
+     * <p>The {@code LinkedList}, on the other hand, uses a doubly linked list under the hood.
+     * It rearranges the pointers of the nodes instead of resizing arrays, making it more efficient
+     * in scenarios where insertions are more frequent than lookups.
+     *
+     * <p>Conversely, {@code ArrayList} is significantly more efficient for searches because it can
+     * directly access the index of the item being searched. In contrast, {@code LinkedList} must
+     * traverse the nodes one by one to find the desired element.
+     *
+     * <p>Reference video for further details:
+     * <a href="https://www.youtube.com/watch?v=5dscMs2hnDI">https://www.youtube.com/watch?v=5dscMs2hnDI</a>
+     */
     public static List<Character> returnAlphabetLetters(int numberOfLetters) {
         final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         List<Character> alphabetLetters = new LinkedList<>();
